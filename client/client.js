8129f626998189abde43d31e791f31c9ebed3ee9
Dan Larson <finsdad@gmail.com>
2014-09-08 10:51:45 -0600

    first commit

---
+++ client/client.js
@@ -0,0 +1,43 @@
+Router.map(function() {
+  this.route('chatapp', {path: '/'});
+  this.route('kitchensink'), {path: '/kitchensink'};
+});
+
+/**
+* Templates
+*/
+// Template.page.showKitchenSink = function () {
+//   return Session.get("showKitchenSink");
+// };
+
+Template.messages.messages = function () {
+    return Messages.find({}, { sort: { time: -1 }});
+}
+
+Template.input.events = {
+    'keydown input#message' : function (event) {
+    if (event.which == 13) { // 13 is the enter key event
+        if (Meteor.user())
+            var name = Meteor.user().profile.name;
+        else
+            var name = 'Anonymous';
+        var message = document.getElementById('message');
+
+        if (message.value != '') {
+            Messages.insert({
+                name: name,
+                message: message.value,
+                time: Date.now(),
+            });
+
+            document.getElementById('message').value = '';
+            message.value = '';
+        }
+    }
+}
+}
+
+// TODO: Figure out how to support multiple pages to show kitchen sink
+Template.page.showKitchenSink = function () {
+    return Session.get("showKitchenSink");
+};
